dbc16b4b4fb010026fffd582dd12ea583b1c116b
Nguyễn Quang Tân <81596255+nguyenquangtan@users.noreply.github.com>
2024-03-14 20:13:34 +0700

    adjust yolox resolution

--- detector/yolox_cfg.py
+++ detector/yolox_cfg.py
@@ -3,6 +3,6 @@ from easydict import EasyDict as edict
 cfg = edict()
 cfg.MODEL_NAME = "yolox-x"
 cfg.MODEL_WEIGHTS = "detector/yolox/data/yolox_x.pth"
-cfg.INP_DIM = 2560
+cfg.INP_DIM = 1920
 cfg.CONF_THRES = 0.3
 cfg.NMS_THRES = 0.7
